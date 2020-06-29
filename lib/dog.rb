a7a4624f9f34b0d7080a43e9428560ed7b742719
William Lawrence <will.lawrence.3rd@gmail.com>
2020-06-29 18:30:04 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,11 @@
 class Dog
-  attr_accessor :mood
-  attr_reader :name
-
-  def initialize(name)
-    @name = name
-    @mood = "nervous"
+  def name=(dogs_name)
+    @this_dogs_name = dogs_name
+  end
+  def name
+    @this_dogs_name
   end
 end
+lassie = Dog.new
+lassie.name = "Lassie"
+puts lassie.name
